86f18844f924538c7cb1f9cbd70d69de9aa01a67
David uribe <Daviduribe0007@gmail.com>
2021-04-21 18:35:57 -0500

    Se crea el value object ProductoExtraibe+

---
+++ src/main/java/co/com/softka/softkau/ddd/domain/inventario/values/ProductoExtraible.java
@@ -0,0 +1,16 @@
+package co.com.softka.softkau.ddd.domain.inventario.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+public class ProductoExtraible implements ValueObject<Boolean> {
+    private  final Boolean value;
+
+    public ProductoExtraible(Boolean value) {
+        this.value = value;
+    }
+
+    @Override
+    public Boolean value() {
+        return value;
+    }
+}
