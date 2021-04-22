3437df765d38f2b0aae0ffb850a5218d21917a7a
David uribe <Daviduribe0007@gmail.com>
2021-04-21 21:00:08 -0500

    Se crea el evento SolicitanteCreado

---
+++ src/main/java/co/com/softka/softkau/ddd/domain/solicitante/SolicitanteChange.java
@@ -0,0 +1,9 @@
+package co.com.softka.softkau.ddd.domain.solicitante;
+
+import co.com.sofka.domain.generic.EventChange;
+
+public class SolicitanteChange extends EventChange {
+    public SolicitanteChange(Solicitante solicitante){
+
+    }
+}
---
+++ src/main/java/co/com/softka/softkau/ddd/domain/solicitante/events/SolicitanteCreado.java
@@ -0,0 +1,38 @@
+package co.com.softka.softkau.ddd.domain.solicitante.events;
+
+import co.com.sofka.domain.generic.DomainEvent;
+import co.com.softka.softkau.ddd.domain.solicitante.values.Estado;
+import co.com.softka.softkau.ddd.domain.solicitante.values.FechaSancion;
+import co.com.softka.softkau.ddd.domain.solicitante.values.Nombre;
+import co.com.softka.softkau.ddd.domain.solicitante.values.Prioridad;
+
+public class SolicitanteCreado extends DomainEvent {
+    private final Nombre nombre;
+    private final Estado estado;
+    private final Prioridad prioridad;
+    private final FechaSancion fechaSancion;
+
+    public SolicitanteCreado(String type, Nombre nombre, Estado estado, Prioridad prioridad, FechaSancion fechaSancion) {
+        super("ddd.solicitante.creado");
+        this.nombre = nombre;
+        this.estado = estado;
+        this.prioridad = prioridad;
+        this.fechaSancion = fechaSancion;
+    }
+
+    public Nombre nombre() {
+        return nombre;
+    }
+
+    public Estado estado() {
+        return estado;
+    }
+
+    public Prioridad prioridad() {
+        return prioridad;
+    }
+
+    public FechaSancion fechaSancion() {
+        return fechaSancion;
+    }
+}
