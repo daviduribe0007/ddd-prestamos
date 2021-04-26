8c173e018ea9a56bef8cec02903b278c1979880c
David uribe <Daviduribe0007@gmail.com>
2021-04-25 20:33:05 -0500

    Se creas un nuevo test en el SolicitanteCreadoUseCase

--- src/main/java/co/com/softka/softkau/ddd/usecase/solicitante/SolicitanteCreadoUseCase.java
+++ src/main/java/co/com/softka/softkau/ddd/usecase/solicitante/SolicitanteCreadoUseCase.java
@@ -1,5 +1,6 @@
 package co.com.softka.softkau.ddd.usecase.solicitante;
 
+import co.com.sofka.business.generic.BusinessException;
 import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.business.support.ResponseEvents;
@@ -9,6 +10,7 @@ import co.com.softka.softkau.ddd.domain.solicitante.events.SolicitanteCreado;
 import co.com.softka.softkau.ddd.domain.solicitante.values.*;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class SolicitanteCreadoUseCase extends UseCase<RequestCommand<CrearSolicitante>, ResponseEvents> {
     @Override
@@ -18,6 +20,11 @@ public class SolicitanteCreadoUseCase extends UseCase<RequestCommand<CrearSolici
     var command= input.getCommand();
     var  solicitante = new Solicitante(command.getEntityId(),command.getNombre(),
             command.getEstado(),command.getPrioridad(),command.getFechaSancion());
+
+        if (0 >= command.getFechaSancion().value().compareTo(Calendar.getInstance())) {
+            throw new BusinessException(command.getEntityId().value(),
+                    "El valor de la fecha de sancion no puede ser posterior a la fecha presente");
+        }
     emit().onResponse(new ResponseEvents(solicitante.getUncommittedChanges()));
 
     }
--- src/test/java/co/com/softka/softkau/ddd/usecase/solicitante/SolicitanteCreadoUseCaseTest.java
+++ src/test/java/co/com/softka/softkau/ddd/usecase/solicitante/SolicitanteCreadoUseCaseTest.java
@@ -1,5 +1,6 @@
 package co.com.softka.softkau.ddd.usecase.solicitante;
 
+import co.com.sofka.business.generic.BusinessException;
 import co.com.sofka.business.generic.UseCaseHandler;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.domain.generic.DomainEvent;
@@ -48,4 +49,26 @@ class SolicitanteCreadoUseCaseTest {
 
     }
 
+    @Test
+    void crearSolicitanteNoValido() {
+        var solicitanteId = new SolicitanteId().of("1234");
+        var nombre = new Nombre("David");
+        var Estado = new Estado(true);
+        var prioridad = new Prioridad(0);
+        Calendar fechaSancion = new GregorianCalendar(1000, 9, 9);
+
+
+        var command = new CrearSolicitante(solicitanteId, nombre,
+                Estado, prioridad, new FechaSancion(fechaSancion));
+        var useCase = new SolicitanteCreadoUseCase();
+
+
+
+        Assertions.assertThrows(BusinessException.class, () -> {
+            UseCaseHandler.getInstance()
+                    .syncExecutor(useCase, new RequestCommand<>(command))
+                    .orElseThrow();
+        }, "El valor de la fecha de sancion no puede ser posterior a la fecha presente");
+
+    }
 }
